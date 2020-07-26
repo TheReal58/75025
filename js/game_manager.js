cc86ebd60f8749defde5a86b4f786189b599a238
TheReal58 <68038765+TheReal58@users.noreply.github.com>
2020-07-26 11:36:20 -0700

    65536 finished!

--- js/game_manager.js
+++ js/game_manager.js
@@ -59,7 +59,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 4;
+    var value = Math.random() < 0.9 ? 1 : 2;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -99,9 +99,20 @@ GameManager.prototype.moveTile = function (tile, cell) {
   tile.updatePosition(cell);
 };
 
+GameManager.prototype.canMerge = function (cellValue1, cellValue2) {
+  // Use heuristics rather than a lookup, because... it's fun.
+  if (cellValue1 === 1 && cellValue2 === 1) return true;
+  if (cellValue1 > cellValue2) {
+     if (cellValue1 <= cellValue2 * 2) return true;
+  } else if (cellValue2 > cellValue1) {
+     if (cellValue2 <= cellValue1 * 2) return true;
+  } 
+  return false;
+};
+
 // Move tiles on the grid in the specified direction
 GameManager.prototype.move = function (direction) {
-  // 0: up, 1: right, 2:down, 3: left
+  // 0: up, 1: right, 2: down, 3: left
   var self = this;
 
   if (this.isGameTerminated()) return; // Don't do anything if the game's over
@@ -126,8 +137,8 @@ GameManager.prototype.move = function (direction) {
         var next      = self.grid.cellContent(positions.next);
 
         // Only one merger per row traversal?
-        if (next && next.value === tile.value && !next.mergedFrom) {
-          var merged = new Tile(positions.next, tile.value * 2);
+        if (next && self.canMerge(next.value, tile.value) && !next.mergedFrom) {
+          var merged = new Tile(positions.next, tile.value + next.value);
           merged.mergedFrom = [tile, next];
 
           self.grid.insertTile(merged);
@@ -139,8 +150,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 65536) self.won = true;
+          // The mighty 75025 tile
+          if (merged.value === 75025) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
